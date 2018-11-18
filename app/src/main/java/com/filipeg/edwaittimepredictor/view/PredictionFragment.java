deed8e0b85d6b503c714855bff4ca12e84e3f40c
phillipsgoncalves <filisgoncalves@gmail.com>
2018-11-18 18:00:43 +0000

    Showing base instructions to time prediction

--- app/src/main/java/com/filipeg/edwaittimepredictor/view/PredictionFragment.java
+++ app/src/main/java/com/filipeg/edwaittimepredictor/view/PredictionFragment.java
@@ -6,9 +6,16 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.CalendarView;
+import android.widget.TimePicker;
 import com.filipeg.edwaittimepredictor.R;
 
 public class PredictionFragment extends BaseFragment implements PredictionView {
+
+  private TimePicker timePicker;
+  private CalendarView calendarView;
+  private Button predictionButton;
 
   @Override public void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -23,9 +30,15 @@ public class PredictionFragment extends BaseFragment implements PredictionView {
 
   @Override public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
     super.onViewCreated(view, savedInstanceState);
+    predictionButton = view.findViewById(R.id.button_predict);
+    timePicker = view.findViewById(R.id.prediction_timestamp);
+    calendarView = view.findViewById(R.id.prediction_calendar);
   }
 
   @Override public void onDestroyView() {
     super.onDestroyView();
+    predictionButton = null;
+    timePicker = null;
+    calendarView = null;
   }
 }
